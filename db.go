3aca25df40478b524d9ed6acf8e5c84dc5a634c3
Rob King <rking188@gmail.com>
2017-08-18 20:33:11 -0400

    Intial functionality for parsing the 'en' local item definitions. The md5 of the manifest is also persisted to allow for caching later.

---
+++ db.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"database/sql"
+
+	_ "github.com/lib/pq" // Only want to import the interface here
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// OutputDB represents the database that the parsed definitions will be saved
+type OutputDB struct {
+	Database *sql.DB
+}
+
+// InputDB represents the input database pulled down from Bungie
+type InputDB struct {
+	Database *sql.DB
+}
+
+var input *InputDB
+var output *OutputDB
+
+// InitOutputDatabase is in charge of preparing any Statements that will be commonly used as well
+// as setting up the database connection pool.
+func InitOutputDatabase() error {
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Println("DB errror: ", err.Error())
+		return err
+	}
+
+	output = &OutputDB{
+		Database: db,
+	}
+
+	return nil
+}
+
+// InitInputDatabase is in charge of preparing any Statements that will be commonly used as well
+// as setting up the database connection pool.
+func InitInputDatabase(dbPath string) error {
+
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		fmt.Println("DB errror: ", err.Error())
+		return err
+	}
+
+	input = &InputDB{
+		Database: db,
+	}
+
+	return nil
+}
+
+// GetOutputDBConnection is a helper for getting a connection to the DB based on
+// environment variables or some other method.
+func GetOutputDBConnection() (*OutputDB, error) {
+
+	if output == nil {
+		fmt.Println("Initializing db!")
+		err := InitOutputDatabase()
+		if err != nil {
+			fmt.Println("Failed to initialize the database: ", err.Error())
+			return nil, err
+		}
+	}
+
+	return output, nil
+}
+
+// GetInputDBConnection is a helper for getting a connection to the DB based on
+// environment variables or some other method.
+func GetInputDBConnection(path string) (*InputDB, error) {
+
+	if input == nil {
+		fmt.Println("Initializing db!")
+		err := InitInputDatabase(path)
+		if err != nil {
+			fmt.Println("Failed to initialize the database: ", err.Error())
+			return nil, err
+		}
+	}
+
+	return input, nil
+}
+
+// ReadManifestChecksums will pull the checksums for the previously parsed
+// manifests to be used for caching. If the manifest checksums match,
+// there have not been any changes.
+func ReadManifestChecksums() (map[string]string, error) {
+
+	out, err := GetOutputDBConnection()
+	if err != nil {
+		fmt.Println("Error getting output database connection to read manifest checksums: ", err.Error())
+		return nil, err
+	}
+
+	result := make(map[string]string)
+
+	rows, err := out.Database.Query("SELECT * FROM manifest_checksums")
+	if err != nil {
+		fmt.Println("Error reading checksums from database: ", err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		var locale string
+		var md5 string
+
+		rows.Scan(&locale, &md5)
+
+		result[locale] = md5
+	}
+
+	return result, nil
+}
+
+// GetItemDefinitions will pull all rows from the input database table
+// with all of the item defintions.
+func (in *InputDB) GetItemDefinitions() (*sql.Rows, error) {
+
+	rows, err := in.Database.Query("SELECT * FROM DestinyInventoryItemDefinition")
+
+	return rows, err
+}
+
+// SaveManifestChecksum is responsible for persisting the checksum for the
+// specified locale to be used for caching later.
+func (out *OutputDB) SaveManifestChecksum(locale, checksum string) error {
+
+	_, err := out.Database.Exec("UPDATE manifest_checksums SET md5=$1 WHERE locale=$2", checksum, locale)
+	return err
+}
+
+// DumpNewItemDefinitions will take all of the item definitions parsed out of the latest
+// manifest from Bungie and write them to the output database to be used for item lookups later.
+func (out *OutputDB) DumpNewItemDefintions(locale, checksum string, definitions []*ItemDefinition) error {
+
+	newTableTempName := fmt.Sprintf("items_%s", checksum)
+
+	// Inside a transaction we need to Insert all item definitions into a new DB and then rename the old db, rename the new one, delete the old one.
+	// TODO: https://dba.stackexchange.com/questions/100779/how-to-atomically-replace-table-data-in-postgresql
+
+	// Create temp new table
+	out.Database.Exec("CREATE TABLE " + newTableTempName + "(LIKE \"items\")")
+	out.Database.Exec("ALTER TABLE " + newTableTempName + " ADD PRIMARY KEY (item_hash)")
+
+	stmt, err := out.Database.Prepare("INSERT INTO " + newTableTempName + " (item_hash, item_name, item_type, item_type_name, tier_type, tier_type_name, class_type, equippable, max_stack_size, display_source, non_transferrable, bucket_type_hash) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)")
+	if err != nil {
+		fmt.Println("Error preparing insert statement: ", err.Error())
+		return err
+	}
+	tx, err := out.Database.Begin()
+	if err != nil {
+		fmt.Println("Error opening transaction to output DB: ", err.Error())
+		return err
+	}
+
+	// Insert all rows into the temp new table
+	txStmt := tx.Stmt(stmt)
+	for _, def := range definitions {
+		_, err = txStmt.Exec(def.ItemHash, strings.ToLower(def.ItemName), def.ItemType, strings.ToLower(def.ItemTypeName), def.TierType, strings.ToLower(def.TierTypeName), def.ClassType, def.Equippable, def.MaxStackSize, def.DisplaySource, def.NonTransferrable, def.BucketTypeHash)
+		if err != nil {
+			fmt.Println("Error inserting item definition: ", err.Error())
+		}
+	}
+
+	// Rename existing table with _old suffix
+	tx.Exec("ALTER TABLE \"items\" RENAME TO \"items_old\"")
+
+	// Rename new temp table with permanent table name
+	tx.Exec("ALTER TABLE " + newTableTempName + " RENAME TO \"items\"")
+
+	// Drop old table
+	tx.Exec("DROP TABLE \"items_old\"")
+
+	// Commit or Rollback if there were errors
+	err = tx.Commit()
+	if err != nil {
+		fmt.Println("Error commiting transaction for inserting new item definitions: ", err.Error())
+		return err
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"archive/zip"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	// BaseURL is the base of all URLs for requesting data from Bungie.
+	BaseURL = "https://www.bungie.net"
+	// ManifestURL is the URL for the manifest spec that will point to the individual manifest DBs.
+	ManifestURL = BaseURL + "/Platform/Destiny/Manifest/"
+)
+
+// ManifestSpecResponse is the response from the public manifest endpoint provided by Bungie.
+// It provides links to all of the content files which are zipped SQLite databases.
+type ManifestSpecResponse struct {
+	Response struct {
+		Version                  string
+		MobileAssetContentPath   string
+		MobileGearAssetDataBases []struct {
+			Version int
+			Path    string
+		}
+		MobileWorldContentPaths map[string]string
+		MobileGearCDN           map[string]string
+	}
+	ErrorCode       int
+	ThrottleSeconds int
+	ErrorStatus     string
+	Message         string
+}
+
+// ManifestRow represents a single row from the input manifest database
+type ManifestRow struct {
+	ID   int
+	JSON string
+}
+
+// ItemDefinition stores all of the fields from the DestinyInventoryItemDefinitions table
+// that we are concerned with.
+type ItemDefinition struct {
+	ItemHash         int    `json:"itemHash"`
+	ItemName         string `json:"itemName"`
+	ItemType         int    `json:"itemType"`
+	ItemTypeName     string `json:"itemTypeName"`
+	TierType         int    `json:"tierType"`
+	TierTypeName     string `json:"tierTypeName"`
+	ClassType        int    `json:"classType"`
+	Equippable       bool   `json:"equippable"`
+	MaxStackSize     int    `json:"maxStackSize"`
+	DisplaySource    string `json:"displaySource"`
+	NonTransferrable bool   `json:"nonTransferrable"`
+	BucketTypeHash   int64  `json:"bucketTypeHash"`
+}
+
+func main() {
+
+	client := http.DefaultClient
+	req, _ := http.NewRequest("GET", ManifestURL, nil)
+
+	bungieAPIKey := os.Getenv("BUNGIE_API_KEY")
+	if bungieAPIKey != "" {
+		// Providing an API Key will decrease the chances of the request being throttled
+		req.Header.Add("X-Api-Key", bungieAPIKey)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Failed to make request to Bungie for the manifest spec")
+		return
+	}
+	defer resp.Body.Close()
+
+	manifestSpec := &ManifestSpecResponse{}
+	err = json.NewDecoder(resp.Body).Decode(manifestSpec)
+	if err != nil {
+		fmt.Println("Error parsing manifest spec response: ", err.Error())
+		return
+	}
+
+	if manifestSpec.ErrorStatus != "Success" || manifestSpec.Message != "Ok" {
+		fmt.Println("Error response from Bungie for manifest spec.")
+		return
+	}
+
+	// manifestChecksums stores the md5sums of the manifest files keyed by the language they
+	// are associated with.
+	manifestChecksums, err := ReadManifestChecksums()
+	if err != nil {
+		fmt.Println("Error trying to read manifest checksums")
+		return
+	}
+
+	for lang, path := range manifestSpec.Response.MobileWorldContentPaths {
+		if lang != "en" {
+			continue
+		}
+		fmt.Println("Checking manifest for language: ", lang)
+		currentChecksum := manifestChecksums[lang]
+		incomingChecksum := checksumFromManifestFilename(path)
+		if currentChecksum != "" && currentChecksum == incomingChecksum {
+			fmt.Println("Incoming manifest is the same as the current one already stored...skipping!")
+			continue
+		}
+
+		sqlitePath := downloadMobileWorldContentPath(path, lang)
+		defer os.Remove(sqlitePath)
+
+		err = processManifestDB(lang, incomingChecksum, sqlitePath)
+	}
+}
+
+// checksumFromManifestFilename parses the md5sum value out of the manifest path
+// example path: /common/destiny_content/sqlite/en/world_sql_content_7d6b460360f589e94baeb8308cada327.content
+func checksumFromManifestFilename(path string) string {
+	underscoreIndex := strings.LastIndex(path, "_")
+	dotIndex := strings.LastIndex(path, ".")
+	if dotIndex == -1 || underscoreIndex == -1 || dotIndex < underscoreIndex {
+		return ""
+	}
+
+	return path[underscoreIndex+1 : dotIndex-1]
+}
+
+// parseMobileWorldContentPath will download and unzip the world content database,
+// unzip it, and save the sqlite database somewhere on disk. The return value
+// is the location on disk where the sqlite database is saved.
+func downloadMobileWorldContentPath(resourcePath, language string) string {
+
+	client := http.DefaultClient
+	req, _ := http.NewRequest("GET", BaseURL+resourcePath, nil)
+
+	strings.TrimSuffix(resourcePath, ".content")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Failed to download manifest for lanuage %s: %s\n", language, err.Error())
+		return ""
+	} else if resp.StatusCode == 304 {
+		fmt.Printf("Manifest file for language %s has not changed... skipping!\n", language)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	// Download the zipped content
+	zipPath := fmt.Sprintf("world_sql_content_%s.content", language)
+	output, err := os.OpenFile(zipPath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Failed to open output file for writing: ", err.Error())
+		return ""
+	}
+	defer output.Close()
+
+	_, err = io.Copy(output, resp.Body)
+	if err != nil {
+		fmt.Println("Failed to write zipped content to disk: ", err.Error())
+		return ""
+	}
+	defer os.Remove(zipPath)
+
+	// Unzip the file
+	zipReader, err := zip.OpenReader(zipPath)
+	if err != nil {
+		fmt.Println("Failed to read zip file that was written to disk: ", err.Error())
+		return ""
+	}
+	defer zipReader.Close()
+
+	if len(zipReader.File) > 1 {
+		fmt.Println("Uh Oh, found more than one file in the manifest zip... ignoring all but the first.")
+	}
+
+	sqliteName := zipReader.File[0].Name
+	zipF, _ := zipReader.File[0].Open()
+
+	sqliteOutput, err := os.OpenFile(sqliteName, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening sqlite file to write to disk: ", err.Error())
+		return ""
+	}
+	defer sqliteOutput.Close()
+	defer zipF.Close()
+
+	io.Copy(sqliteOutput, zipF)
+
+	// Return the path to the unzipped SQLite DB
+	return sqliteName
+}
+
+// processManifestDB will process the manifest sqlite database for the specified Locale,
+// reading the desired fields out of the manifest and inserting them into the new relational DB.
+// The checksum provided is the md5 of the SQLite database file being processed. This should
+// be stored when the new table is written to provide caching support next time.
+func processManifestDB(locale, checksum, sqlitePath string) error {
+
+	input, err := GetInputDBConnection(sqlitePath)
+	if err != nil {
+		fmt.Println("Error opening the input database: ", err.Error())
+		return err
+	}
+	defer input.Database.Close()
+	_, err = GetOutputDBConnection()
+	if err != nil {
+		fmt.Println("Error opening output database: ", err.Error())
+		return err
+	}
+
+	inRows, err := input.GetItemDefinitions()
+	if err != nil {
+		fmt.Println("Error reading item definitions from sqlite: ", err.Error())
+		return err
+	}
+	defer inRows.Close()
+
+	itemDefs := make([]*ItemDefinition, 0)
+	for inRows.Next() {
+		row := ManifestRow{}
+		inRows.Scan(&row.ID, &row.JSON)
+
+		item := ItemDefinition{}
+		json.Unmarshal([]byte(row.JSON), &item)
+
+		itemDefs = append(itemDefs, &item)
+	}
+
+	fmt.Printf("Processed %d item definitions\n", len(itemDefs))
+
+	err = output.DumpNewItemDefintions(locale, checksum, itemDefs)
+	if err == nil {
+		output.SaveManifestChecksum(locale, checksum)
+	}
+
+	return err
+}
